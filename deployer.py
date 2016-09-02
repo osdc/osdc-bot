29baaaddcf663f96a56a8b6d5548d4a962c1188a
Yash Mehrotra <yashmehrotra95@gmail.com>
2016-09-02 16:50:28 +0530

    Updated Deployer

--- deployer.py
+++ deployer.py
@@ -7,12 +7,14 @@ def deploy():
     screen_name = "osdc_bot"
     screen_kill_comm = "screen -S {} -X quit".format(screen_name)
     screen_start_comm = 'screen -S "{}" -d -m'.format(screen_name)
+    update_local_comm = 'screen -r "{0}" -X stuff "{1}"'.format(screen_name, "git pull origin master\n")
     start_bot_comm = 'screen -r "{0}" -X stuff "{1}"'.format(screen_name, "nodejs chatbot.js\n")
     try:
         soldier.run(screen_kill_comm)
     except:
         pass
 
+    print(soldier.run(update_local_comm).status_code)
     print(soldier.run(screen_start_comm).status_code)
     print(soldier.run(start_bot_comm).status_code)
     return 'Deployed'
