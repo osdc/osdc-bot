c458d6d6301e5d9dd242796447af541755eabdd6
Avijit Gupta <526avijit@gmail.com>
2016-09-02 22:04:17 +0530

    Major code refactoring

--- chatbot.js
+++ chatbot.js
@@ -9,20 +9,34 @@ const TOKEN   = config.token;
 
 const DEPLOY_FLAG = process.env.DEPLOY || false;
 
+const META_HANDSHAKE_SUFFIX_URL = '/meta/handshake';
+const FAYE_CLIENT_URL = 'https://ws.gitter.im/faye';
+const SERVER_PREFIX_URL = "http://127.0.0.1:5000";
+const SERVER_DEPLOY_URL = `${SERVER_PREFIX_URL}/deploy`;
+const SERVER_HOWDOI_PREFIX_URL = `${SERVER_PREFIX_URL}/howdoi?query=`;
+const SEND_TO_CHAT_URL = `https://api.gitter.im/v1/rooms/${ROOM_ID}/chatMessages`;
+
+const BOT_MENTION_NAME = "@osdc-bot";
+const BOT_ACTIONS = {
+  HELP: 'help',
+  JOKE: 'joke',
+  DEPLOY: 'deploy',
+  HOWDOI: 'howdoi'
+};
+
 // Authentication extension
 var ClientAuthExt = function() {};
 
 ClientAuthExt.prototype.outgoing = (message, callback) => {
-  if (message.channel === '/meta/handshake') {
+  if (message.channel === META_HANDSHAKE_SUFFIX_URL) {
     if (!message.ext) { message.ext = {}; }
     message.ext.token = TOKEN;
   }
-
   callback(message);
 };
 
 ClientAuthExt.prototype.incoming = (message, callback) => {
-  if(message.channel === '/meta/handshake') {
+  if(message.channel === META_HANDSHAKE_SUFFIX_URL) {
     if(message.successful) {
       console.log('Successfuly subscribed to room: ', ROOM_ID);
       if (DEPLOY_FLAG) {
@@ -32,19 +46,22 @@ ClientAuthExt.prototype.incoming = (message, callback) => {
       console.log('Something went wrong: ', message.error);
     }
   }
-
   callback(message);
 };
 
-
 // Faye client
-const client = new Faye.Client('https://ws.gitter.im/faye', {timeout: 60, retry: 5, interval: 1});
+const clientOptions =  {
+  timeout: 60,
+  retry: 5,
+  interval: 1
+};
+const client = new Faye.Client(FAYE_CLIENT_URL, clientOptions);
 
 // Add Client Authentication extension
 client.addExtension(new ClientAuthExt());
 
 // A dummy handler to echo incoming messages
-var messageHandler = (msg) => {
+const messageHandler = (msg) => {
   if (msg.model && msg.model.fromUser) {
     console.log("Message: ", msg.model.text);
     console.log("From: ", msg.model.fromUser.displayName);
@@ -53,44 +70,49 @@ var messageHandler = (msg) => {
   }
 };
 
+function _getStartsWith(parsedMessage) {
+  var result = null;
+  for (var botAction in BOT_ACTIONS) {
+    if (parsedMessage.startsWith(BOT_ACTIONS[botAction])) {
+      result = BOT_ACTIONS[botAction];
+      console.log(`[INFO] In loop {result}`);
+      break;
+    }
+  }
+}
 
 function reply_to_user(user, message) {
   const displayName = user.displayName;
   const username = user.username;
 
-  if (message.lastIndexOf("@osdc-bot") === 0) {
-    const data = message.slice(10, message.length);
-    console.log(data);
-    if (data.lastIndexOf("help") === 0) {
-      // list all commands
-      console.log("[INFO] In help loop");
+  if (message.startsWith(BOT_MENTION_NAME)) {
+    const parsedMessage = message.slice(BOT_MENTION_NAME.length + 1);
+    console.log(parsedMessage);
+    const startsWithString = _getStartsWith(parsedMessage);
+    if (startsWithString === BOT_ACTIONS.HELP) {
       send("You can:\n- joke\n- deploy\n", username);
     }
 
-    if (data.lastIndexOf("joke") === 0) {
-      console.log("[INFO] In joke loop");
+    if (startsWithString === BOT_ACTIONS.JOKE) {
       joker.getJoke(send, username);
     }
 
-    if (data.lastIndexOf("deploy") === 0) {
-      console.log("[INFO] Deploy");
+    if (startsWithString === BOT_ACTIONS.DEPLOY) {
       request({
-        url: "http://127.0.0.1:5000/deploy",
+        url: SERVER_DEPLOY_URL,
         method: "GET"
       }, (error, response, body) => {
         console.log(response);
       });
     }
 
-    if (data.lastIndexOf("howdoi") === 0) {
-      const query = encodeURIComponent(data.slice(7, data.length));
+    if (startsWithString === BOT_ACTIONS.HOWDOI) {
+      var query = encodeURIComponent(parsedMessage.slice(7, parsedMessage.length));
       console.log(query);
-      console.log("[INFO] howdoi");
       request({
-        url: `http://127.0.0.1:5000/howdoi?query=${query}`,
+        url: SERVER_HOWDOI_PREFIX_URL + query,
         method: "GET"
       }, (error, response, body) => {
-        console.log('HowdoI request successful');
         console.log(body);
         send(body);
       });
@@ -100,13 +122,15 @@ function reply_to_user(user, message) {
 
 function _postOnChat(message) {
   request({
-    url: `https://api.gitter.im/v1/rooms/${ROOM_ID}/chatMessages`,
+    url: SEND_TO_CHAT_URL,
     headers: {
       Authorization : `Bearer ${TOKEN}`
     },
     method: "POST",
     json: true,
-    body: {text: message}
+    body: {
+      text: message
+    }
   }, (error, response, body) => {
     console.log(response);
   });
