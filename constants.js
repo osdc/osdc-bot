46cccab70fd1a32536d9e09298700b848ff8910f
Avijit Gupta <526avijit@gmail.com>
2016-09-04 10:04:58 +0530

    Modify package details, travis versions, config.js

--- constants.js
+++ constants.js
@@ -4,16 +4,6 @@ const config = require('./config');
 const ROOM_ID  = config.roomId;
 const TOKEN   = config.token;
 
-const META_HANDSHAKE_SUFFIX_URL = '/meta/handshake';
-const FAYE_CLIENT_URL = 'https://ws.gitter.im/faye';
-const SERVER_PREFIX_URL = "http://127.0.0.1:5000";
-const SERVER_DEPLOY_URL = `${SERVER_PREFIX_URL}/deploy`;
-const SERVER_HOWDOI_PREFIX_URL = `${SERVER_PREFIX_URL}/howdoi?query=`;
-const CHATROOM_SUFFIX_URL = `/v1/rooms/${ROOM_ID}/chatMessages`;
-const CHATROOM_URL = `https://api.gitter.im${CHATROOM_SUFFIX_URL}`;
-const JOKES_API_URL = 'http://api.icndb.com/jokes/random';
-const CLIENT_SUBSCRIBE_URL = `/api${CHATROOM_SUFFIX_URL}`;
-
 const BOT_MENTION_NAME = "@osdc-bot";
 const BOT_ACTIONS = {
   HELP: 'help',
@@ -22,18 +12,28 @@ const BOT_ACTIONS = {
   HOWDOI: 'howdoi'
 };
 
+const META_HANDSHAKE_SUFFIX_URL = '/meta/handshake';
+const FAYE_CLIENT_URL = 'https://ws.gitter.im/faye';
+const CHATROOM_SUFFIX_URL = `/v1/rooms/${ROOM_ID}/chatMessages`;
+const CLIENT_SUBSCRIBE_URL = `/api${CHATROOM_SUFFIX_URL}`;
+const CHATROOM_URL = `https://api.gitter.im${CHATROOM_SUFFIX_URL}`;
+const SERVER_PREFIX_URL = "http://127.0.0.1:5000";
+const SERVER_DEPLOY_URL = `${SERVER_PREFIX_URL}/deploy`;
+const SERVER_HOWDOI_PREFIX_URL = `${SERVER_PREFIX_URL}/howdoi?query=`;
+const JOKES_API_URL = 'http://api.icndb.com/jokes/random';
+
 module.exports = {
-  META_HANDSHAKE_SUFFIX_URL,
-  FAYE_CLIENT_URL,
-  SERVER_PREFIX_URL,
-  SERVER_DEPLOY_URL,
-  SERVER_HOWDOI_PREFIX_URL,
-  CHATROOM_SUFFIX_URL,
-  CHATROOM_URL,
-  CLIENT_SUBSCRIBE_URL,
-  JOKES_API_URL,
   BOT_MENTION_NAME,
   BOT_ACTIONS,
   ROOM_ID,
-  TOKEN
+  TOKEN,
+  META_HANDSHAKE_SUFFIX_URL,
+  FAYE_CLIENT_URL,
+  CHATROOM_SUFFIX_URL,
+  CLIENT_SUBSCRIBE_URL,
+  CHATROOM_URL,
+  SERVER_PREFIX_URL,
+  SERVER_DEPLOY_URL,
+  SERVER_HOWDOI_PREFIX_URL,
+  JOKES_API_URL
 };
