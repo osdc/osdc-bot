2bd698b8075df73b55d859fc61a0b457beea4427
priyansh gupta <priyanshgupta1161@gmail.com>
2016-09-04 22:17:23 +0530

    style inprovement

--- services/wikiService.js
+++ services/wikiService.js
@@ -1,29 +1,29 @@
 var request = require('request');
 var WIKI_PREFIX_URL =  'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=4&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
 
-module.exports = function (cb,username,input){
+module.exports = function (callback,username,input){
   'use strict';
   let apiOptions = {
     url :`${WIKI_PREFIX_URL}${input}`,
     method : 'GET',
     json : {}
   };
-  request(apiOptions,(err,response,data)=>{
-    if(!err && response.statusCode==200){
-      let result = '',dataArr=[];
+  request(apiOptions,(err,response,data) => {
+    if(!err && response.statusCode == 200){
+      let result = '', dataArr=[];
       if(data.query){
         for(var key in data.query.pages){
-          dataArr[data.query.pages[key].index-1 ]=data.query.pages[key]; //counting sort .
+          if(data.query.pages.hasOwnProperty(key))
+            dataArr[data.query.pages[key].index-1 ]=data.query.pages[key]; //counting sort .
         }
       }
       dataArr.forEach((curr,index)=>{
-        result+=`\n ## ${index+1}[${curr.title}](https://en.wikipedia.org/?curid=${curr.pageid})\n ${curr.extract}\n`;
+        result+=`\n ### ${ index+1 }[${ curr.title }](https://en.wikipedia.org/?curid=${ curr.pageid })\n ${ curr.extract }\n`;
       });
-      console.log(result);
-      cb(result,username);
+      callback(result,username);
     }
     else {
-      cb(err ?err.message :'unable to find',username);
+      callback(err ?err.message :'unable to find',username);
     }
   });
 };
