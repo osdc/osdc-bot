742989f85193027a0fa95f22bdb48de6a3285992
Yash Mehrotra <yashmehrotra95@gmail.com>
2016-09-10 04:42:57 +0530

    Fix lint

--- services/karmaService.js
+++ services/karmaService.js
@@ -4,39 +4,46 @@ const config = require('../config');
 const db = new sqlite3.Database(config.dbFilePath);
 
 const _parseAllUserKarma = (callback, rows) => {
-  let msg = "Karma Table: \n";
+  let msg = 'Karma Table: \n';
   for (let row in rows) {
-    msg += `${rows[row].username}: ${rows[row].totalKarma}\n`
+    msg += `${rows[row].username}: ${rows[row].totalKarma}\n`;
   }
   callback(msg);
-}
+};
 
 const _getUserKarma = (username, callback, message) => {
-  db.each("SELECT SUM(point) as totalKarma FROM karma WHERE username = ?", username,
-    (err, row) => {
-      return callback(message + row.totalKarma);
-  });
-}
+  db.each(
+    'SELECT SUM(point) as totalKarma FROM karma WHERE username = ?', username,
+    (error, row) => {
+      if (!error) {
+        return callback(message + row.totalKarma);
+      }
+    });
+};
 
 module.exports = {
   getKarma: (callback) => {
-    let userKarmaMapping = {};
-    db.all("SELECT SUM(point) as totalKarma, username FROM karma GROUP BY username ORDER BY totalKarma DESC",
-      (err, rows) => {
-        _parseAllUserKarma(callback, rows);
-    });
+    db.all(
+      ('SELECT SUM(point) as totalKarma, username FROM karma' +
+       'GROUP BY username ORDER BY totalKarma DESC'),
+      (error, rows) => {
+        if (!error) {
+          _parseAllUserKarma(callback, rows);
+        }
+      });
   },
   giveKarma: (callback, username, value) => {
-    if ([-1,1].indexOf(value) > -1) {
+    if ([-1, 1].indexOf(value) > -1) {
       // Insert into DB
       db.serialize(function() {
-        db.run("INSERT INTO karma (username, point) VALUES($username, $point)", {
-          $username: username,
-          $point: value,
-        });
+        db.run(
+         'INSERT INTO karma (username, point) VALUES($username, $point)', {
+           $username: username,
+           $point: value
+         });
         let message = `Karma of ${username} is: `;
         _getUserKarma(username, callback, message);
       });
     }
-  },
+  }
 };
