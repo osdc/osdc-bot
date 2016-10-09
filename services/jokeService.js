58294c8bd4774dfe09ff7515f30feaf293daa414
Yash <yash17agrawal@gmail.com>
2016-10-09 08:54:23 +0530

    Fixed linting issues

--- services/jokeService.js
+++ services/jokeService.js
@@ -4,15 +4,16 @@ const constants = require('../constants');
 
 module.exports = {
   getJoke: (callback, username) => {
-    var index = Math.floor(Math.random() * constants.JOKES_API_URL.length);
-    var randomQuote =  constants.JOKES_API_URL[index];
+    const index = Math.floor(Math.random() * constants.JOKES_API_URL.length);
+    const randomQuote =  constants.JOKES_API_URL[index];
     request(randomQuote, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-	if (index==0)
-	  return callback(JSON.parse(body).value.joke, username);
-	else
-	  return callback(JSON.parse(body).joke, username);      
-	}
+      if (!error && response.statusCode === 200) {
+        if (index === 0) {
+          return callback(JSON.parse(body).value.joke, username);
+        } else {
+          return callback(JSON.parse(body).joke, username);      
+        }
+      }
     });
   }
 };
